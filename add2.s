13304bc51c7ef0fe8e3af777a3b17590067c4f6e
Peter McGarrigle <mcgarrigle@github.com>
2018-02-12 09:27:27 +0000

    added syscall.h

--- add2.s
+++ add2.s
@@ -10,7 +10,7 @@ format: .asciz  "sum is %d = %d + %d\n"
 .text
 .global main
 
-main:   ldr     r0, =format    @ printf("sum is %d = %d + %d\n", r1, r2 ,r3)
+main:   ldr     r0, =format    // printf("sum is %d = %d + %d\n", r1, r2 ,r3)
         mov     r2, #123
         mov     r3, #111
         add     r1, r2, r3
